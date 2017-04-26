ea7bd3ae0fffeaca096e2abdca648ca1b8753c19
Sergio Uve <sergiouve@gmail.com>
2017-04-26 16:27:23 +0200

    Add proper anchor link to documentation

--- views/pages/welcome.view.php
+++ views/pages/welcome.view.php
@@ -25,7 +25,7 @@
     <h1>Hidro is up and running</h1>
 
     <p>Delete this and start prototyping!</p>
-    <p>Doubts? <a href="https://github.com/sergiouve/hidro">Check this</a></p>
+    <p>Doubts? <a href="https://github.com/sergiouve/hidro#usage">Check this</a></p>
 
   </div>
 </div>
